4ce0641fb637afc80c668e39582e1a89bb7a4d75
Official-Triranjan26 <triranjansamanta@gmail.com>
2024-01-27 00:32:42 +0530

    clint setup navbar foodtab done

---
+++ client/src/pages/Restaurant.page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import RestaurantLayout from '../layouts/Restaurant.layout'
+
+const Restaurant = () => {
+  return (
+    <div>Restaurant</div>
+  )
+}
+
+export default Restaurant;
